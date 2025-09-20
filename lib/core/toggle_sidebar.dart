2fa66b3d70640b579ceae8f6d356b8f8c237848c
Erick <erickmaraza20@gmail.com>
2025-09-19 22:06:33 -0500

    feat(ui): add toggle_sidebar.dart

---
+++ lib/core/toggle_sidebar.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+
+class ToggleSidebar extends StatelessWidget {
+  const ToggleSidebar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return PopupMenuButton<int>(
+      color: const Color(0xFF242424),
+      offset: const Offset(0, 40),
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(12),
+      ),
+      
+      icon: Image.asset(
+        "assets/sidebar.png",
+        width: 28,
+        height: 28,
+        color: const Color(0xFFF0E8D5), 
+      ),
+      onSelected: (value) {
+        if (value == 0) {
+         
+        } else if (value == 1) {
+          
+        } else if (value == 2) {
+         
+        }
+      },
+      itemBuilder: (context) => [
+        _buildMenuItem("assets/subscription.png", "Subscription", 0),
+        _buildMenuItem("assets/support.png", "Support", 1),
+        _buildMenuItem("assets/logout.png", "Log out", 2),
+      ],
+    );
+  }
+
+  PopupMenuItem<int> _buildMenuItem(String iconPath, String text, int value) {
+    return PopupMenuItem<int>(
+      value: value,
+      child: Row(
+        children: [
+          Image.asset(
+            iconPath,
+            width: 22,
+            height: 22,
+            color: const Color(0xFFF0E8D5),
+          ),
+          const SizedBox(width: 12),
+          Text(
+            text,
+            style: GoogleFonts.josefinSans(
+              fontSize: 16,
+              color: const Color(0xFFF0E8D5),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
