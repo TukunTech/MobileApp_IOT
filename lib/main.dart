8b5f9498883f04247da21f4b1f5e3f74359c85c5
Erick <erickmaraza20@gmail.com>
2025-09-23 03:29:09 -0500

    feat(router): add routes for ProfilePage, SupportPage and SubscriptionPage

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,9 @@ import 'package:flutter/material.dart';
 import 'package:tukuntech/features/home/presentation/pages/home_page.dart';
 import 'package:tukuntech/features/login/presentation/login_page.dart';
 import 'package:tukuntech/features/register/presentation/register_page.dart';
-
+import 'package:tukuntech/features/profile/presentation/profile_page.dart';
+import 'package:tukuntech/features/support/presentation/pages/support_page.dart';
+import 'package:tukuntech/features/subscription/presentation/pages/subscription_page.dart';
 void main() {
   runApp(const MyApp());
 }
@@ -13,16 +15,19 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-  debugShowCheckedModeBanner: false,
-  title: 'Tukuntech',
-  theme: ThemeData(useMaterial3: true),
-  initialRoute: '/login',
-  routes: {
-    '/login': (_) => const LoginPage(),
-    '/register': (_) => const RegisterPage(),
-    '/home': (_) => const HomePage(),
-
-  },
-);
+      debugShowCheckedModeBanner: false,
+      title: 'Tukuntech',
+      theme: ThemeData(useMaterial3: true),
+      initialRoute: '/login',
+      routes: {
+        '/login': (_) => const LoginPage(),
+        '/register': (_) => const RegisterPage(),
+        '/home': (_) => const HomePage(),
+        '/profile': (_) => const ProfilePage(),
+        '/support':(_) => const SupportPage(),
+        '/subscription': (context) => const SubscriptionPage(),
+      },
+    );
   }
 }
+
